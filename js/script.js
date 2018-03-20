d056c5c98ec172666c7d87855a85e32196bdb3e9
matheus <matheusviannavieira@hotmail.com>
2018-03-20 13:07:34 -0300

    Mudança na img

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  $("#sobre hr.easteregg").dblclick(function(){
+    alert("Feito com amor, para o meu amor !")
+  });
+
+  $("#slider").owlCarousel({
+    loop:true,
+    autoplay:true,
+    center:true,
+    margin:75,
+    //autoWidth:true,
+    responsiveClass:true,
+    responsive:{
+      0:{
+        items: 1
+      },
+      480:{
+        items: 1
+      },
+      768:{
+        items: 1
+      },
+      1024:{
+        items: 2
+      },
+      1440:{
+        items: 3
+      }
+    }
+  });
+
+  $(".owl-stage").on("mousedown", function(){
+    $(this).addClass(".owl-stage");
+  }).on("mouseup", function(){
+    $(this).removeClass(".owl-stage");
+  });
+});
